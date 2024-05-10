6ed1d4876871ac1e52cb1e7c035c3635f87ece1c
Musibau Moyosore Alao <73694764+Moyoalao@users.noreply.github.com>
2024-05-11 00:20:23 +0100

    Initial Commit

---
+++ Recommendation/RecommendationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecommendationApp.swift
+//  Recommendation
+//
+//  Created by Musibau Alao on 11/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct RecommendationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
